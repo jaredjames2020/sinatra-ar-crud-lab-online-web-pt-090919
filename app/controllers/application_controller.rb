da280a519f79b1a20b70c95c7040b22b3dd0b30d
Jared S <jaredjames2020@gmail.com>
2019-12-24 21:22:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,44 @@
 
+# require_relative '../../config/environment'
+
+# class ApplicationController < Sinatra::Base
+
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+#   end
+
+#   get '/articles' do
+#     @articles = Article.all
+#     erb :index
+#   end
+  
+#   # get '/' do
+#   #   erb :index
+#   # end
+  
+#   get '/articles/new' do
+#     erb :new
+#   end 
+  
+#   post '/articles' do 
+#     @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+#     redirect "/articles/#{@article.id}"
+#   end
+  
+#   get 'articles/:id' do
+#     @article = Article.find(params[:id])
+#     erb :show
+#     binding.pry
+#   end
+  
+#   get 'articles/:id/edit' do
+#     @article = Article.find(params[:id])
+#     erb :edit
+#   end
+
+# end
+
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,7 +47,39 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  get '/' do
+  
+  get '/articles' do 
+    @articles = Article.all 
+    erb :index
   end
+  
+  get '/articles/new' do 
+    erb :new
+  end
+  
+  post '/articles' do 
+    @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+    redirect "/articles/#{@article.id}"
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    Article.update(params[:id], params[:article])
+    redirect "/articles/#{id}"
+  end
+  
+  delete '/articles/:id' do
+    Article.delete(params[:id])
+    redirect '/articles'
+  end
+  
 end
